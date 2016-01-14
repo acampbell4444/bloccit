5a372127c0890cc007d22f46e5caf308f6a40a85
Alan Campbell <alancampbell4444@gmail.com>
2016-01-13 18:44:28 -0800

    checkpoint-26-testing : complete

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -4,8 +4,15 @@ RSpec.describe WelcomeController, type: :controller do
   describe "GET index" do
     it "renders the index template" do
       get :index
-      expect(response.to render_template("index")
+      expect(response).to render_template("index")
     end
-  end
+   end
+
+   describe "GET about" do
+     it "renders the about template" do
+       get :about
+       expect(response).to render_template("about")
+     end
+   end
 
 end

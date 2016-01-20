f96dcea874daf7e8b3ec6ad2b47488e70f358a3a
Alan Campbell <alancampbell4444@gmail.com>
2016-01-19 22:30:33 -0800

    checkpoint-39-post_and_users : complete

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -4,11 +4,11 @@ class TopicsController < ApplicationController
   end
 
   def edit
-      @topic = Topic.find(params[:id])
+    @topic = Topic.find(params[:id])
   end
 
   def show
-     @topic = Topic.find(params[:id])
+   @topic = Topic.find(params[:id])
   end
 
   def new
@@ -17,44 +17,44 @@ class TopicsController < ApplicationController
 
 
   def create
-    @topic = Topic.new
-    @topic.name = params[:topic][:name]
-    @topic.description = params[:topic][:description]
-    @topic.public = params[:topic][:public]
+    @topic = Topic.new(topic_params)
 
     if @topic.save
       redirect_to @topic, notice: "Topic was saved successfully."
-     else
-       flash.now[:alert] = "Error creating topic. Please try again."
-       render :new
-     end
-   end
+    else
+      flash.now[:alert] = "Error creating topic. Please try again."
+      render :new
+    end
+  end
 
-   def update
-   @topic = Topic.find(params[:id])
+  def update
+    @topic = Topic.find(params[:id])
+    @topic.assign_attributes(topic_params)
 
-   @topic.name = params[:topic][:name]
-   @topic.description = params[:topic][:description]
-   @topic.public = params[:topic][:public]
-
-   if @topic.save
-     flash[:notice] = "Topic was updated."
-     redirect_to @topic
-   else
-     flash.now[:alert] = "Error saving topic. Please try again."
-     render :edit
-   end
+    if @topic.save
+    flash[:notice] = "Topic was updated."
+    redirect_to @topic
+    else
+    flash.now[:alert] = "Error saving topic. Please try again."
+    render :edit
+    end
   end
 
   def destroy
-  @topic = Topic.find(params[:id])
+    @topic = Topic.find(params[:id])
 
-  if @topic.destroy
-    flash[:notice] = "\"#{@topic.name}\" was deleted successfully."
-    redirect_to action: :index
-  else
-    flash.now[:alert] = "There was an error deleting the topic."
-    render :show
+    if @topic.destroy
+      flash[:notice] = "\"#{@topic.name}\" was deleted successfully."
+      redirect_to action: :index
+    else
+      flash.now[:alert] = "There was an error deleting the topic."
+      render :show
+    end
   end
+
+  private
+
+  def topic_params
+    params.require(:topic).permit(:name, :description, :public)
   end
 end

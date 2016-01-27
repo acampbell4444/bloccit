1144efbe303571e14e2c6d79820151b70e188a4d
Alan Campbell <alancampbell4444@gmail.com>
2016-01-26 17:57:26 -0800

    update

--- config/routes.rb
+++ config/routes.rb
@@ -22,12 +22,12 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
 
-  namespace :api do
-     namespace :v1 do
-       resources :users, only: [:index, :show]
-       resources :users, only: [:index, :show, :create, :update]
-       resources :topics, except: [:edit, :new]
 
-     end
-   end
+
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:index, :show, :create, :update]
+      resources :topics, except: [:edit, :new]
+    end
+  end
 end
